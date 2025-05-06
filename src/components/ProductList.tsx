ac330a65cdc8a1d429f0a79162b6e95f008a1a50
Willy Budiman Tedjakusuma <willy.tedjakusuma@gmail.com>
2025-05-06 23:43:37 +0800

    fix: Product grid typo

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -22,7 +22,7 @@ const ProductList: React.FC = () => {
   return (
     <div className='p-4'>
       <h2 className='text-xl font-bold mb-4'>Product List</h2>
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-3 gap-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
         {products.map((product) => (
           <div 
             key={product.id}
